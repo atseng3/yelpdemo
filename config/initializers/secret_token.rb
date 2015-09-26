6ce9a9a6ac54fd21e647f323e7f1f386f26b56e7
Albert Tseng <albert@chegg.com>
2015-09-25 18:02:50 -0700

    yelpdemo first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yelpdemo::Application.config.secret_key_base = 'd8333e878195eda0d947da62129b43eb7357b87ea660eac46ed52835cdcbdecb83f1dad6bc69ba24a58b4184ed67ea78e3d5d812983ae65db4bb8cd6b648d674'
