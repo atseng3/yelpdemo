b49c03e21f3c7ce4ea7e2d9afc5f4027bb3d1ddf
Albert Tseng <albert@chegg.com>
2015-09-27 10:34:34 -0700

    making reviews nested under restaurants and adding restaurant_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Yelpdemo::Application.routes.draw do
-  resources :reviews, except: [:show, :index]
-
   devise_for :users
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, except: [:show, :index]  
+  end
 
   get "pages/about"
   get "pages/contact"
